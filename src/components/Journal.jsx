179503a1ffe409113b7e5a12d3940284f3a0c435
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-18 20:08:25 +0200

    Add props to the Journal components

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 export default function Journal(props){
-    console.log(props.location)
+    console.log(props.imageUrl)
     return (
         <div className="flex gap-4 p-4 border-b-gray-100 border-b-2">
             <div className="w-[25%]">
-                <img src="/images/locationOne-trial.svg" alt="" className="object-fill w-full" />
+                <img src={`${props.imageUrl}`} alt="" className="object-fill w-full" />
             </div>
             <div className="w-[75%] py-4">
                 <div className="flex flex-col pb-4">
                     <div className="flex items-center text-sm">
                         <img src="/images/location-fill.svg" alt="location logo" className="w-3" />
                         <span className="uppercase ml-1 mr-6">Japan</span>
-                        <a href="" className="text-gray-400 underline">View on Google Maps</a>
+                        <a href={`${props.googleMapsUrl}`} className="text-gray-400 underline">View on Google Maps</a>
                     </div>
-                    <h1 className="font-bold text-4xl">Mount Fuji</h1>
+                    <h1 className="font-bold text-4xl text-gray-800">{props.title}</h1>
                 </div>
-                <p className="font-bold mb-1">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className=" text-xs">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <p className="font-bold mb-1">{props.startDate} - {props.endDate}</p>
+                <p className=" text-xs">{props.description}</p>
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/12/Monte_Fuji.JPG"
+        imageUrl: "https://images.unsplash.com/photo-1576086213369-97f6d1d8b266"
     },
     {
         title: "Eiffel Tower",
